009596643bf9f164a5588d5d9d6ff284de3766f5
johng <internet.user.personal@gmail.com>
2024-01-22 22:00:07 -0800

    added mail to implicit intent.

--- app/src/main/java/com/androidapps/thehungrydeveloper/MainActivity.java
+++ app/src/main/java/com/androidapps/thehungrydeveloper/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         startersCard = findViewById(R.id.card_view_starters);
         mainsCard = findViewById(R.id.card_view_mains);
+
+
 
         startersCard.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,5 +42,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(mainCoursesActivity);
             }
         });
+
+        TextView emailTextView = findViewById(R.id.text_view_email_address);
+        emailTextView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent launchEmailAppIntent = new Intent(Intent.ACTION_SENDTO);
+                launchEmailAppIntent.setData(Uri.parse("mailto:"));
+                startActivity(launchEmailAppIntent);
+            }
+        });
+
     }
 }
